d53cb22551bd940d26cac1998d9403eff01999ac
Federico Schmidt <schmidt.federico@hotmail.com>
2020-05-09 11:21:46 -0300

    Package basic structure.

---
+++ apschedulerui/__init__.py
@@ -0,0 +1,4 @@
+__project__ = "apschedulerui"
+__author__ = "Federico Schmidt"
+__version__ = "0.0.1"
+__license__ = "MIT"
---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+dependencies = [
+    'apscheduler >= 3.0.0'
+]
+
+setup(
+    name='apschedulerui',
+    version=VERSION,
+    description='',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Federico Schmidt',
+    author_email='schmidt.fdr@gmail.com',
+    url='https://github.com/schmidtfederico/apscheduler-ui',
+    packages=find_packages(exclude=['tests']),
+    install_requires=dependencies,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        "Programming Language :: Python :: 3.5",
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Operating System :: OS Independent',
+        'Topic :: Internet'
+    ],
+    python_requires='>=3.5, <4',
+    extras_require={
+        'testing:python_version == "3.5"': ['mock']
+    }
+)
